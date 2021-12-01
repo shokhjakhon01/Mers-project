f829268e2c6b6864ac7a216f342b4431e52b6d31
Shohjahon <suyunovshohjahon08@gmail.com>
2021-12-01 18:49:10 +0500

    Alhamdulillah

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
+require("es6-promise").polyfill();
+import "nodelist-foreach-polyfill";
 import tabs from "./modules/tabs";
-  import data from "./modules/datas";
-  import modal from "./modules/modal";
-  import accordion from "./modules/accordion";
-  import classes from "./modules/class";
-  import loader from "./modules/loader";
-  import slider from "./modules/slider";
-  import {openModal} from './modules/modal'
+import data from "./modules/datas";
+import modal from "./modules/modal";
+import accordion from "./modules/accordion";
+import classes from "./modules/class";
+import loader from "./modules/loader";
+import slider from "./modules/slider";
+import { openModal } from "./modules/modal";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const modalTimer = setTimeout(()=> openModal('.modal', modalTimer), 50000);
+  const modalTimer = setTimeout(() => openModal(".modal", modalTimer), 50000);
 
   tabs();
   data();
-  modal('[data-modal]', ".modal", modalTimer);
+  modal("[data-modal]", ".modal", modalTimer);
   accordion(modalTimer);
   classes();
   loader();
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,30 @@ module.exports = {
     path: __dirname + "/js",
   },
   watch: true,
-  devtool: 'source-map',
-  module: {}
+  devtool: "source-map",
+  module: {},
 };
+
+module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules\bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage",
+              },
+            ],
+          ],
+        },
+      },
+    },
+  ];
+}
